edd20eb2444e1b73ab606482a462fdb1dd060cd2
Pouria Mellati <pouria@pouria.me>
2019-04-30 01:23:10 +1000

    Add code to escape scala identifiers.

--- src/main/scala/kubegen/Main.scala
+++ src/main/scala/kubegen/Main.scala
@@ -213,4 +213,72 @@ object Main {
   
   def packageOf(fullyQualifiedName: String): String =
     fullyQualifiedName.split('.').init.mkString(".")
+  
+}
+
+object Scala {
+  /** Escape the input in back-ticks if it's not a valid scala identifier.
+   * 
+   * Class, object, variable & method names are called identifiers.
+   */
+  def ident(s: String): String =
+    if(isValidIdent(s)) s
+    else                s"`$s`"
+
+  /**
+   * Class, object, variable & method names are called identifiers.
+   */
+  def isValidIdent(s: String): Boolean = {
+    val validIdentChars = """[a-zA-Z_$][\w_$]+"""
+    val alreadyEscaped  = """`[^`]+`"""
+
+    s.matches(alreadyEscaped) || (
+      s.matches(validIdentChars) && !isReservedWord(s)
+    )
+  }
+
+  def isReservedWord: String => Boolean =
+    ReservedWords.contains
+
+  final val ReservedWords = List(
+    "abstract",
+    "case",
+    "catch",
+    "class",
+    "def",
+    "do",
+    "else",
+    "extends",
+    "false",
+    "final",
+    "finally",
+    "for",
+    "forSome",
+    "if",
+    "implicit",
+    "import",
+    "lazy",
+    "match",
+    "new",
+    "null",
+    "object",
+    "override",
+    "package",
+    "private",
+    "protected",
+    "return",
+    "sealed",
+    "super",
+    "this",
+    "throw",
+    "trait",
+    "try",
+    "true",
+    "type",
+    "val",
+    "var",
+    "while",
+    "with",
+    "yield"
+  )
 }
