6e63f379a4b3f564d309709f20ade94ad7a71e3e
Pouria Mellati <pouria@pouria.me>
2019-05-12 21:30:40 +1000

    Move the Scala object to a separate file.

--- src/main/scala/kubegen/Main.scala
+++ src/main/scala/kubegen/Main.scala
@@ -11,7 +11,9 @@ import cats.implicits._
 import io.swagger.parser.SwaggerParser
 import io.swagger.models._, parameters._, properties._
 
-import Scala._
+import kubegen.ScalaStringContext.ScalaStringContextImplicit
+import kubegen.ScalaCode._, syntax._
+import kubegen.Scala._
 
 object Main {
   def main(args: Array[String]) {
@@ -243,75 +245,4 @@ object Main {
   
   def packageOf(fullyQualifiedName: String): String =
     fullyQualifiedName.split('.').init.mkString(".")
-}
-
-object Scala {
-  /** Escape the input in back-ticks if it's not a valid scala identifier.
-   * 
-   * Class, object, variable & method names are called identifiers.
-   */
-  def ident(s: String): String =
-    if(isValidIdent(s)) s
-    else                s"`$s`"
-
-  /** Sanitise a fully qualified name. */
-  def sanitiseFqn(s: String): String =
-    s.split('.').toList.map(ident).mkString(".")
-
-  /**
-   * Class, object, variable & method names are called identifiers.
-   */
-  def isValidIdent(s: String): Boolean = {
-    val validIdentChars = """[a-zA-Z_$][\w_$]+"""
-    val alreadyEscaped  = """`[^`]+`"""
-
-    s.matches(alreadyEscaped) || (
-      s.matches(validIdentChars) && !isReservedWord(s)
-    )
-  }
-
-  def isReservedWord: String => Boolean =
-    ReservedWords.contains
-
-  final val ReservedWords = List(
-    "abstract",
-    "case",
-    "catch",
-    "class",
-    "def",
-    "do",
-    "else",
-    "extends",
-    "false",
-    "final",
-    "finally",
-    "for",
-    "forSome",
-    "if",
-    "implicit",
-    "import",
-    "lazy",
-    "match",
-    "new",
-    "null",
-    "object",
-    "override",
-    "package",
-    "private",
-    "protected",
-    "return",
-    "sealed",
-    "super",
-    "this",
-    "throw",
-    "trait",
-    "try",
-    "true",
-    "type",
-    "val",
-    "var",
-    "while",
-    "with",
-    "yield"
-  )
 }
---
+++ src/main/scala/kubegen/Scala.scala
@@ -0,0 +1,72 @@
+package kubegen
+
+object Scala {
+  /** Escape the input in back-ticks if it's not a valid scala identifier.
+   * 
+   * Class, object, variable & method names are called identifiers.
+   */
+  def ident(s: String): String =
+    if(isValidIdent(s)) s
+    else                s"`$s`"
+
+  /** Sanitise a fully qualified name. */
+  def sanitiseFqn(s: String): String =
+    s.split('.').toList.map(ident).mkString(".")
+
+  /**
+   * Class, object, variable & method names are called identifiers.
+   */
+  def isValidIdent(s: String): Boolean = {
+    val validIdentChars = """[a-zA-Z_$][\w_$]+"""
+    val alreadyEscaped  = """`[^`]+`"""
+
+    s.matches(alreadyEscaped) || (
+      s.matches(validIdentChars) && !isReservedWord(s)
+    )
+  }
+
+  def isReservedWord: String => Boolean =
+    ReservedWords.contains
+
+  final val ReservedWords = List(
+    "abstract",
+    "case",
+    "catch",
+    "class",
+    "def",
+    "do",
+    "else",
+    "extends",
+    "false",
+    "final",
+    "finally",
+    "for",
+    "forSome",
+    "if",
+    "implicit",
+    "import",
+    "lazy",
+    "match",
+    "new",
+    "null",
+    "object",
+    "override",
+    "package",
+    "private",
+    "protected",
+    "return",
+    "sealed",
+    "super",
+    "this",
+    "throw",
+    "trait",
+    "try",
+    "true",
+    "type",
+    "val",
+    "var",
+    "while",
+    "with",
+    "yield"
+  )
+}
